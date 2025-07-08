3500a0e025b4b20232a8bb17f6598733b35b03d8
KumariAnjali <kumarianjali206700@gmail.com>
2025-07-09 02:21:12 +0530

    added the dashboard and login page

--- client/src/components/admin/Sidebar.jsx
+++ client/src/components/admin/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 
         <NavLink to='/admin/listBlog' className={({isActive})=> `flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-64 cursor-pointer ${isActive && " bg-primary/10 border-r-4 border-primary " }`}> 
         <img src={assets.list_icon} alt="" className='min-w-4 w-5'/>
-        <p className='hidden md:inline-block '>List Blogs</p>
+        <p className='hidden md:inline-block '> Blogs List</p>
         </NavLink>
 
         <NavLink to='/admin/comments' className={({isActive})=> `flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-64 cursor-pointer ${isActive && " bg-primary/10 border-r-4 border-primary " }`}> 
